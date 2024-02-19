b95b75aa79c325b9c540c79b774969903da59ae0
Xin Liu <sam@secondstate.io>
2024-02-20 00:29:23 +0800

    chore: remove unused code
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/main.rs
+++ src/main.rs
@@ -32,77 +32,8 @@ async fn main() -> Result<(), String> {
     // * load and chunk the text file
     let chunks = load_and_chunk_text(file)?;
 
-    {
-        // let file_path = Path::new(&file);
-        // if !file_path.exists() {
-        //     println!("File {} does not exist", file_path.display());
-        //     return Err("File does not exist".to_string());
-        // }
-        // if file_path.extension().is_none() || file_path.extension().unwrap() != "txt" {
-        //     println!("File {} is not a text file", file_path.display());
-        //     return Err("File is not a text file".to_string());
-        // }
-
-        // // read contents from a text file
-        // let mut file = File::open(file_path).expect("failed to open file");
-        // let mut text = String::new();
-        // file.read_to_string(&mut text).expect("failed to read file");
-
-        // // println!("File contents: {}", text);
-
-        // // * split text into chunks
-
-        // println!("[+] Chunking the text ...");
-        // let tokenizer = cl100k_base().unwrap();
-        // let max_tokens = 100;
-        // let splitter = TextSplitter::new(tokenizer).with_trim_chunks(true);
-
-        // let chunks = splitter.chunks(&text, max_tokens).collect::<Vec<_>>();
-
-        // // for chunk in chunks.iter() {
-        // //     println!("\nlen: {}, contents: {}\n", chunk.len(), chunk);
-        // // }
-    }
-
     // * create embeddings
     let embeddings = llama_compute_embeddings(&chunks).await?;
-
-    {
-        // println!("[+] Creating embeddings for the chunks ...");
-        // // let input = chunks.iter().map(|x| x.to_string()).collect();
-        // let embedding_request = EmbeddingRequest {
-        //     model: "dummy-embedding-model".to_string(),
-        //     input: chunks,
-        //     encoding_format: None,
-        //     user: None,
-        // };
-        // let request_body = serde_json::to_value(&embedding_request).unwrap();
-
-        // // create a client
-        // let client = reqwest::Client::new();
-        // let embeddings = match client
-        //     .post("http://localhost:8080/v1/embeddings")
-        //     .header("accept", "application/json")
-        //     .header("Content-Type", "application/json")
-        //     .json(&request_body)
-        //     .send()
-        //     .await
-        // {
-        //     Ok(response) => {
-        //         let embedding_reponse: EmbeddingsResponse = response.json().await.unwrap();
-        //         println!(
-        //             "    * Number of embedding objects: {}",
-        //             embedding_reponse.data.len()
-        //         );
-
-        //         embedding_reponse.data
-        //     }
-        //     Err(err) => {
-        //         println!("Error: {}", err);
-        //         return Err(err.to_string());
-        //     }
-        // };
-    }
 
     // * access qdrant db
 
@@ -117,53 +48,6 @@ async fn main() -> Result<(), String> {
 
     // create and upsert points
     qdrant_persist_embeddings(&qdrant_client, collection_name, &embeddings, &chunks).await?;
-
-    {
-        // println!("[+] Creating points to save embeddings ...");
-        // let mut points = Vec::<Point>::new();
-        // for embedding in embeddings.iter() {
-        //     // convert the embedding to a vector
-        //     let vector: Vec<_> = embedding.embedding.iter().map(|x| *x as f32).collect();
-
-        //     // create a payload
-        //     let payload = serde_json::json!({"source": &chunks[embedding.index as usize]})
-        //         .as_object()
-        //         .map(|m| m.to_owned());
-
-        //     // create a point
-        //     let p = Point {
-        //         id: PointId::Num(embedding.index),
-        //         vector,
-        //         payload,
-        //     };
-
-        //     points.push(p);
-        // }
-        // let dim = points[0].vector.len();
-
-        // // create a Qdrant client
-        // let qdrant_client = qdrant::Qdrant::new();
-
-        // // Create a collection with `dim`-dimensional vectors
-        // println!("[+] Creating a collection ...");
-        // let collection_name = "my_test";
-        // println!("    * Collection name: {}", collection_name);
-        // println!("    * Dimension: {}", dim);
-        // if let Err(err) = qdrant_client
-        //     .create_collection(collection_name, dim as u32)
-        //     .await
-        // {
-        //     println!("Failed to create collection. {}", err.to_string());
-        //     return Err(err.to_string());
-        // }
-
-        // // upsert points
-        // println!("[+] Upserting points ...");
-        // if let Err(err) = qdrant_client.upsert_points(collection_name, points).await {
-        //     println!("Failed to upsert points. {}", err.to_string());
-        //     return Err(err.to_string());
-        // }
-    }
 
     // * compute embeddings for a query
 
